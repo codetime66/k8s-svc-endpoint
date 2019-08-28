2e9945fdc1b794d5dfb55dd8dbb797bf60d9fde0
codetime <codetime66@gmail.com>
2019-08-28 19:19:44 -0300

    sample

---
+++ run-sample.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./bin/k8s-svc-endpoint --kubeconfig ~/projects/kubeland/hzubernetes/.kube/conf --service-name api/v1/namespaces/credenciamento/endpoints/credenciamento-validacao-telefone
